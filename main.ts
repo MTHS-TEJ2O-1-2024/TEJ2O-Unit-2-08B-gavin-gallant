0c207e32ef86dab0cba7853f13f5e918af9cb589
gavin-gallant <gavin.gallant@mths.ca>
2024-10-07 10:41:27 -0400

    merge update

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: oct 2024
+ * This program when we hit A, all of the colors of an RGB LED light up in sequence
 */
 
 // Clear the display
